3868956301e53a231afc9bc3572d8770ff1c0cb7
shishao-in139 <shishaoqi0928@gmail.com>
2018-06-16 11:16:20 +0800

    first commit: create huobi Api

---
+++ src/HuobiServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace shishao\huobiApi;
+
+use shishao\huobiApi\Huobi\HuobiApi;
+
+class HuobiServiceProvider extends ServiceProvider
+{
+    /**
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/huobi.php' => config_path('huobi.php'),
+            ], 'my-huobi');
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerModelFactory();
+    }
+
+    /**
+     * Register Model ModelFactory.
+     *
+     * @return void
+     */
+    protected function registerModelFactory()
+    {
+        $this->app->singleton(HuobiApi::class, function ($app) {
+            return new HuobiApi(config('huobi.account_id', 'huobi.access_key', 'huobi.secret_key'));
+        });
+    }
+
+    /**
+     * @return array
+     */
+    public function provides()
+    {
+        return [HuobiApi::class];
+    }
+}
---
+++ src/config/huobi.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Configurations
+    |--------------------------------------------------------------------------
+    |
+    | In this section you may define the default configuration for each model
+    | that will be generated from any database.
+    |
+    */
+
+    'huobi' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | 账户信息
+        |--------------------------------------------------------------------------
+        |
+        |
+        */
+
+        /*define('ACCOUNT_ID', ''); // 你的账户ID
+        define('ACCESS_KEY',''); // 你的ACCESS_KEY
+        define('SECRET_KEY', ''); // 你的SECRET_KEY*/
+        'account_id' => '',
+        'access_key' => '',
+        'secret_key' => '',
+    ]
+];
